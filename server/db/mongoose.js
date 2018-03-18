14ec59f8085bbc715ed6ab61c05fee76eb41d10f
DynamicFlash <aldrin.a.fernandes.com>
2018-03-18 19:50:36 +0530

    this is for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 mongoose.Promise =global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports= {mongoose};
